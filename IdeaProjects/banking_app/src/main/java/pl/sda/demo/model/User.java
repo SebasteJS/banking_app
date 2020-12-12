a0e274609e592a4d09caf61b32690f66b12ac3fc
Urbi-jd <67183953+Urbi-jd@users.noreply.github.com>
2020-12-12 10:17:22 +0100

    user entity, login and passwodr in customer added

---
+++ IdeaProjects/banking_app/src/main/java/pl/sda/demo/model/User.java
@@ -0,0 +1,20 @@
+package pl.sda.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "rola")
+public class Rola {
+
+    @Id
+    
+}
