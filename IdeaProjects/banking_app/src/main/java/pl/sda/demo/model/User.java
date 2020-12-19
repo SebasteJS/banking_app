d98677fd0f0f24735affdb2748fea023a68d227e
Krzysiek-lab <krzyho.91@o2.pl>
2020-12-19 01:04:49 +0100

    security z kontrollerami i userami i unikalnym uzytkownikiem i defaultowa rola 7

--- IdeaProjects/banking_app/src/main/java/pl/sda/demo/model/User.java
+++ IdeaProjects/banking_app/src/main/java/pl/sda/demo/model/User.java
@@ -39,7 +39,4 @@ public class User {
     @JoinTable(name = "user_role", joinColumns = {@JoinColumn(name = "user_id")}, inverseJoinColumns = {@JoinColumn(name = "role_id")})
     private List<Role> roles;
 
-
-
-
 }
