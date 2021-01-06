1ddc67d3c8415ca64d34befb3f27896e336b3489
Krzysiek-lab <krzyho.91@o2.pl>
2021-01-06 19:01:38 +0100

    Merge1 (security,logic,templates)2

--- IdeaProjects/banking_app/src/main/java/pl/sda/demo/controller/MainController.java
+++ IdeaProjects/banking_app/src/main/java/pl/sda/demo/controller/MainController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+class MainController {
+
     @GetMapping({"/", "/index"})
     public String mainPage() {
         return "index";
