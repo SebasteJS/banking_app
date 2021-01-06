417cbedb36ba8a045eb26e98a386997b0f7ce16e
SebasteJS <sebastian.ksiel.js@gmail.com>
2021-01-06 19:41:43 +0100

    commit

--- IdeaProjects/banking_app/src/main/java/pl/sda/demo/controller/mainController.java
+++ IdeaProjects/banking_app/src/main/java/pl/sda/demo/controller/mainController.java
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class mainController {
-
+public class MainController {
     @GetMapping({"/", "/index"})
     public String mainPage() {
         return "index";
